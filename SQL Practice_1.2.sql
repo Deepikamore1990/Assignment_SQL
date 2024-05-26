13c3eadbb1925a633d7a5d7d4f5536ac28ac4b38
Deepika Rakesh Khedekar <108405811+Deepikamore1990@users.noreply.github.com>
2024-05-26 22:10:36 +0530

    Add files via upload

--- SQL Practice_1.2.sql
+++ SQL Practice_1.2.sql
@@ -1,6 +1,7 @@
 Use Deepika;
 # Filtering Data Using WHERE and Sorting the Output
-SELECT movie_title, imdb_rating, year_released
+SELECT
+  movie_title, imdb_rating, year_released
 FROM movies
 WHERE year_released > 1991
 ORDER BY year_released ASC;
