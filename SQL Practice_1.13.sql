72d3b2e67da723f0dfc8d6dc28f2d44b5f89bc2d
Deepika Rakesh Khedekar <108405811+Deepikamore1990@users.noreply.github.com>
2024-06-06 23:20:51 +0530

    Add files via upload

---
+++ SQL Practice_1.13.sql
@@ -0,0 +1,9 @@
+use Deepika;
+#Insert Data:
+#Insert at least 5 records into the Employees table.
+INSERT INTO Employee (EmployeeID, FirstName, LastName, HireDate, Salary) VALUES
+(1, 'John', 'Doe', '2020-01-15', 60000),
+(2, 'Jane', 'Smith', '2019-03-10', 75000),
+(3, 'Alice', 'Johnson', '2021-07-23', 50000),
+(4, 'Bob', 'Brown', '2018-11-30', 82000),
+(5, 'Charlie', 'Davis', '2017-05-19', 68000);
